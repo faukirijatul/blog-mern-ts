2ef188e95a5d1e44a79912bc97865346837c3963
faukirijatul <faukirijatul42@gmail.com>
2025-01-28 16:36:36 +0700

    uninstall multer

--- server/src/api/v1/routes/blog.route.ts
+++ server/src/api/v1/routes/blog.route.ts
@@ -1,15 +1,11 @@
 import express from "express";
 import { createBlog, getBlogBySlug, updateBlogBySlug } from "../controllers/blog.controller";
 import { checkAuthAndRefreshToken } from "../../../middlewares/checkAuthAndRefreshToken";
-import multer from "multer";
-
-const storage = multer.memoryStorage();
-export const upload = multer({ storage });
 
 const route = express.Router();
 
-route.post("/", checkAuthAndRefreshToken, upload.single("thumbnail"), createBlog);
+route.post("/", checkAuthAndRefreshToken, createBlog);
 route.get("/:slug", getBlogBySlug);
-route.put("/:slug", checkAuthAndRefreshToken, upload.single("thumbnail"), updateBlogBySlug);
+route.put("/:slug", checkAuthAndRefreshToken, updateBlogBySlug);
 
 export default route;
